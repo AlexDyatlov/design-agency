b2bfe4ab0a8cf59e891a3aede0e3be4e1c309af3
Alyx11 <aleks78.gooogle@gmail.com>
2020-05-20 01:42:31 +0300

    added adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,36 @@ $(function(){
     slidesPerView: 3,
     spaceBetween: 35,
     loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
+    breakpoints: {
+      320: {
+        slidesPerView: '1',
+        spaceBetween: 35,
+      },
+      951: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      1350: {
+        slidesPerView: 3,
+        spaceBetween: 35,
+      },
+    }
   })
 
-});
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 
+});
 
   var show = true;
   var countbox = "#counts";
@@ -39,6 +60,7 @@ $(function(){
       show = false;
 
     }
+
   });
 
 
